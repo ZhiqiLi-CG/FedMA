0a63bb1c8c82a15cf40930f47951fabeea021c8e
ZhiqiLi-CG <zhiqilicg@gmail.com>
2023-09-21 16:51:24 +0800

    first commit

---
+++ run.sh
@@ -0,0 +1,15 @@
+python main.py --model=moderate-cnn \
+--dataset=cifar10  \
+--lr=0.01  \
+--retrain_lr=0.01  \
+--batch-size=64  \
+--epochs=20  \
+--retrain_epochs=10  \
+--n_nets=16  \
+--partition=hetero-dir  \
+--comm_type=fedma  \
+--comm_round=6  \
+--retrain=True  \
+--rematching=True \
+--prune_rate=0 \
+--seed=0
